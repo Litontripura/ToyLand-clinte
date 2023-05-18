9c123e3e71b2dada63b5c60fc02795226b0f8c0e
Litontripura <litontripura01@gmail.com>
2023-05-18 16:06:48 +0600

    i have created the add toys page

--- src/pages/AddToys/AddToys.jsx
+++ src/pages/AddToys/AddToys.jsx
@@ -1,11 +1,108 @@
 import React from 'react';
 
-const AddToys = () => {
-    return (
-        <div>
-            <h1>Add toys</h1>
-        </div>
-    );
+const ToyForm = () => {
+
+    const handleSubmit =(event)=>{
+        event.preventDefault()
+        const form = event.target;
+        const photo = form.toysphoto.value;
+        const name = form.name.value;
+        const sellerName =form.sellerName.value;
+        const sellerEmail = form.sellerEmail.value;
+        const category = form.category.value;
+        const price = form.price.value;
+        const rating = form.rating.value;
+        const availablequantity = form.rating.value;
+        const description = form.description.value;
+        const userInfo = {
+            photo, name, sellerName, sellerEmail, category, price, rating, availablequantity, description
+        }
+        fetch('http://localhost:5000/toys',{
+            method:"POST",
+            headers:{
+                "content-type":"application/json"
+            },
+            body:JSON.stringify(userInfo)
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data);
+        })
+        console.log(userInfo);
+
+    }
+
+  return (
+  
+    <>
+      <form onSubmit={handleSubmit} className="bg-white">
+      <div className='relative md:grid grid-cols-2 gap-6  shadow-md rounded px-8 pt-6 pb-8 mb-4 md:w-1/2 lg:w-1/2 mx-auto'>
+      <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2" >
+          Toys Picture
+        </label>
+        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="toysPhoto"  type="text" name='toysphoto' placeholder="Enter your toys photo url" />
+      </div>
+      <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+          Name
+        </label>
+        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" name='name' type="text" placeholder="Name" />
+      </div>
+      <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="seller-name">
+          Seller Name
+        </label>
+        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="seller-name" name='sellerName' type="text" placeholder="Seller Name" />
+      </div>
+      <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="seller-email">
+          Seller Email
+        </label>
+        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="seller-email" name='sellerEmail' type="email" placeholder="Seller Email" />
+      </div>
+      <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="sub-category">
+          Sub-category
+        </label>
+        <select name='category' className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="sub-category">
+          <option >Math Toys</option>
+          <option className='my-2'>Language Toys</option>
+          <option>Science Toys</option>
+        </select>
+      </div>
+      <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="price">
+          Price
+        </label>
+        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="price" name='price' type="text" placeholder="Price" />
+      </div>
+      <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="rating">
+          Rating
+        </label>
+        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="rating" type="number" name='rating' step="0.1" min="0" max="5" placeholder="Rating" />
+      </div>
+      <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2" >
+          Available Quantity
+        </label>
+        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="quantity" type="number" name='availablequantity' min="0" placeholder="Available Quantity" />
+      </div>
+      <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="description">
+          Description
+        </label>
+        <textarea className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="description" name='description' placeholder="Add toys description" rows="5" />
+      </div>
+      </div>
+       <div className='text-center absolute bottom-0'>
+       <button className=' ' type="submit">Add toy</button>
+       </div>
+    </form>
+    
+    </>
+  );
 };
 
-export default AddToys;
+export default ToyForm;
